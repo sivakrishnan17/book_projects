7971489fde0c6a9b452392c5901365a6facbfc46
sivakrishnan17 <siva000krishnan@gmail.com>
2025-01-08 12:54:04 +0530

    add code

---
+++ alien_invasion.py
@@ -0,0 +1,11 @@
+# create a pygame window and responding to user input:
+import sys
+import pygame
+class AlienInvasion:
+    '''Overall class to manage game assets and behaviour.'''
+    def __init__(self):
+        
+
+
+
+
