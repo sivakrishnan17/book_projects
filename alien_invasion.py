ebfa57b36d1136ec1148ba79bd6b551b9f8db0ee
sivakrishnan17 <siva000krishnan@gmail.com>
2025-01-08 14:00:04 +0530

    update alien file

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ class AlienInvasion:
     '''Overall class to manage game assets and behaviour.'''
     def __init__(self):
         pygame.init()
+        self.clock=pygame.time.Clock()
         self.screen=pygame.display.setmode(1200,800)
         pygame.display.set_caption("Alien Invasion")
     def run_game(self):
@@ -16,7 +17,7 @@ class AlienInvasion:
                     sys.exit()
             # make the most recently drawn screen visible.
             pygame.display.flip()
-
+            self.clock.tick(60)
 
 
 
