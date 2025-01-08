eaa1c0be8ec540b3118f6557ab96de3ea61e6789
sivakrishnan17 <siva000krishnan@gmail.com>
2025-01-08 13:21:47 +0530

    update alien file and automatic create tempcoderunner file

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,21 @@ import pygame
 class AlienInvasion:
     '''Overall class to manage game assets and behaviour.'''
     def __init__(self):
-        
+        pygame.init()
+        self.screen=pygame.display.setmode(1200,800)
+        pygame.display.set_caption("Alien Invasion")
+    def run_game(self):
+        '''Start the main loop for the game.'''
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    sys.exit()
+            # make the most recently drawn screen visible.
+            pygame.display.flip()
+
+
+
 
 
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if __name__=='__main__':
+     # make a game instance,and run the game.
+    ai.AlienInvasion()
+    ai.run_game()
