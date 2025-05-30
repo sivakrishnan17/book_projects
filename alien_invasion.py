0479015698588e9135aae1608a0f099e54cdf5bf
sivakrishnan17 <siva000krishnan@gmail.com>
2025-05-30 16:58:59 +0530

    edit alien and delete extra file

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,7 +6,7 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.clock=pygame.time.Clock()
-        self.screen=pygame.display.setmode(1200,800)
+        self.screen=pygame.display.set_mode(1200,800)
         pygame.display.set_caption("Alien Invasion")
     def run_game(self):
         '''Start the main loop for the game.'''
@@ -18,7 +18,10 @@ class AlienInvasion:
             # make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
-
+if __name__=='__main__':
+     # make a game instance,and run the game.
+    ai=AlienInvasion()
+    ai.run_game()
 
 
 
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-if __name__=='__main__':
-     # make a game instance,and run the game.
-    ai.AlienInvasion()
-    ai.run_game()
