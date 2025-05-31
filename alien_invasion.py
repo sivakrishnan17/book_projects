aa11d7296dd52cfad36baf51fa7f7296d01a8521
sivakrishnan17 <siv000krishnan@gmail.com>
2025-05-31 21:30:44 +0530

    edit code

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,8 @@ class AlienInvasion:
         self.clock=pygame.time.Clock()
         self.screen=pygame.display.set_mode(1200,800)
         pygame.display.set_caption("Alien Invasion")
+        # setr color
+        self.bg_color=(230,230,230)
     def run_game(self):
         '''Start the main loop for the game.'''
         while True:
@@ -16,12 +18,14 @@ class AlienInvasion:
                 if event.type==pygame.QUIT:
                     sys.exit()
             # make the most recently drawn screen visible.
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
             self.clock.tick(60)
 if __name__=='__main__':
      # make a game instance,and run the game.
     ai=AlienInvasion()
     ai.run_game()
+
 
 
 
